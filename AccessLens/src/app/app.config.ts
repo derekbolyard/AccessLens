45c482659838fece740929c05864a7f6277669a6
derek bolyard <derekbolyard@gmail.com>
2025-06-09 00:26:29 -0400

    theme

--- AccessLens/src/app/app.config.ts
+++ AccessLens/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
-    provideAnimations(),
     provideRouter(routes)
   ]
 };
