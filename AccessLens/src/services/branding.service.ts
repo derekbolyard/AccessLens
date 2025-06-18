07a7e3d8741f27507b52807d5e386766bd93a691
derekbolyard <32370477+derekbolyard@users.noreply.github.com>
2025-06-17 20:09:02 -0400

    Improve branding feature

--- AccessLens/src/services/branding.service.ts
+++ AccessLens/src/services/branding.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 import { BrandingInfo } from '../types/branding.interface';
 
 @Injectable({ providedIn: 'root' })
 export class BrandingService {
   private brandingSubject = new BehaviorSubject<BrandingInfo[]>([]);
   public branding$ = this.brandingSubject.asObservable();
+  private apiUrl = `${environment.apiUrl}/branding`;
 
-  getBranding(): Observable<BrandingInfo[]> {
-    return this.branding$;
-  }
+  constructor(private http: HttpClient) {}
 
-  createBranding(info: BrandingInfo): void {
-    const list = [...this.brandingSubject.value, info];
-    this.brandingSubject.next(list);
-  }
-
-  updateBranding(id: string, update: Partial<BrandingInfo>): void {
-    const list = this.brandingSubject.value.map(b =>
-      b.id === id ? { ...b, ...update } : b
+  loadBranding(): Observable<BrandingInfo[]> {
+    return this.http.get<BrandingInfo[]>(this.apiUrl).pipe(
+      tap(list => this.brandingSubject.next(list))
     );
-    this.brandingSubject.next(list);
   }
 
-  deleteBranding(id: string): void {
-    const list = this.brandingSubject.value.filter(b => b.id !== id);
-    this.brandingSubject.next(list);
+  createBranding(data: FormData): Observable<BrandingInfo> {
+    return this.http.post<BrandingInfo>(this.apiUrl, data).pipe(
+      tap(b => this.brandingSubject.next([...this.brandingSubject.value, b]))
+    );
+  }
+
+  updateBranding(id: string, data: FormData): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, data).pipe(
+      tap(() => {
+        this.loadBranding().subscribe();
+      })
+    );
+  }
+
+  deleteBranding(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        const list = this.brandingSubject.value.filter(b => b.id !== id);
+        this.brandingSubject.next(list);
+      })
+    );
   }
 }
