a96d6a1ee3aa47d73a6b055eed4f1a5048c8989e
derek bolyard <derekbolyard@gmail.com>
2025-06-19 19:13:46 -0400

    config

--- AccessLens/src/main.ts
+++ AccessLens/src/main.ts
@@ -5,8 +5,10 @@ import { APP_INITIALIZER } from '@angular/core';
 import { CsrfService } from './services/csrf.service';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
 
 export function initCsrf(csrf: CsrfService) {
+  console.info('BUILD MODE:', environment.production ? 'prod' : 'dev');
   // Angular calls this _before_ the first component.
   return () => csrf.init();        // returns Promise<void>
 }
