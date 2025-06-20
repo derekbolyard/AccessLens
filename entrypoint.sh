192e1da4c24b1509a0672fc5c460cc485ef6e9c2
derek bolyard <derekbolyard@gmail.com>
2025-06-20 19:09:28 -0400

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-/usr/local/bin/minio server /data --address :9000 --console-address :9001 &
-dotnet AccessLensApi.dll
+echo "[ENTRYPOINT] Starting AccessLens API"
+exec dotnet AccessLensApi.dll
